affd2888272a7052c1111036a1cd1574bea676f9
chenzhongyou <chenzhongyou@topjoy.com>
2024-03-21 19:08:59 +0800

    feat：add notification

---
+++ mobile/src/main/java/com/example/sms/manager/notification/NotificationManager.kt
@@ -0,0 +1,18 @@
+package com.example.sms.manager.notification
+
+/**
+ * 通知操作类
+ */
+class NotificationManager {
+
+    /**
+     * todo
+     * 1. 自定义icon
+     * 2. 自定义文本内容、
+     * 3. 自定义点击事件
+     */
+    companion object{
+
+    }
+
+}
